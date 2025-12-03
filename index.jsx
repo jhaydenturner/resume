d251f362a6b94e8e500f2a8c7be0ae7aca488d9d
jhaydenturner <jhaydenturner@gmail.com>
2025-12-03 10:32:21 -0700

    Initial project setup. Includes necessary dependencies. Creates basic PDF upon build.

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import fs from "fs";
+import { renderToBuffer } from "@react-pdf/renderer";
+import Resume from "./resume";
+
+const resumeName = "resume.pdf";
+
+async function main() {
+  const pdfBuffer = await renderToBuffer(<Resume />);
+  fs.writeFileSync(resumeName, pdfBuffer);
+}
+main();
