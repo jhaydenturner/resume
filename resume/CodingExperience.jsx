60d3828223922538997a6c396eb8e6d4f4afb01e
jhaydenturner <jhaydenturner@gmail.com>
2025-12-05 17:32:59 -0700

    Minor grammar update.

--- resume/CodingExperience.jsx
+++ resume/CodingExperience.jsx
@@ -11,7 +11,7 @@ export default function CodingExperience() {
       <List>
         <ListMarker info="Worked on a team of 5-6 developers in a startup to build a clean, efficient SaaS product. Simplified complex shipping issues for 3PLs and direct shippers by providing a well rounded platform for managing shipments, viewing data, and billing customers." />
         <ListMarker info="Fully responsible for development of several major features: insights dashboard, document center to upload/download files, custom client reports delivered via email, and PDF autoparser tool to extract data from contracts." />
-        <ListMarker info="Maintained and enhanced existing complex features in the platform such as shipment and carrier invoice detail pages, spreadsheet tools to input data from tables of anolog/digital files, and collection of data from external sources via custom scripts. Also modernized color palette and implemented throughout the product." />
+        <ListMarker info="Maintained and enhanced existing complex features in the platform such as shipment and carrier invoice detail pages, spreadsheet tools to input data from tables of analog/digital files, and collection of data from external sources via custom scripts. Also modernized color palette and implemented throughout the product." />
       </List>
       <Subheading
         title="Software Engineer Intern at Nascent Labs"
